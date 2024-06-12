5d98e2fefaf3180f90bf8b7bb9998a54276a60cf
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-06-12 15:44:02 -0300

    add .env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDBz91nn3JkxYxEDw-yBl1l35DUytGYlM0",
-  authDomain: "restaurant-app-rn-cc857.firebaseapp.com",
-  projectId: "restaurant-app-rn-cc857",
-  storageBucket: "restaurant-app-rn-cc857.appspot.com",
-  messagingSenderId: "198648484398",
-  appId: "1:198648484398:web:7043bed7dd97bebdd505bf",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
